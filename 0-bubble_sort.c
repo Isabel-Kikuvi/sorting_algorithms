931d09b4a892a501355911ca0be626fc3701b138
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-06-25 00:50:22 -0700

    updates

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,11 +10,14 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int temp;
+	int temp, flag;
+
+	if (size < 2 || array == NULL)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-
+		flag = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -22,9 +25,12 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				flag = 1;
 			}
-			print_array(array, size);
+		print_array(array, size);
 		}
+		if (flag == 0)
+			break;
 	}
 
 }
