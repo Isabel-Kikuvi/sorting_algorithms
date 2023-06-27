9839a0d6570581faceb99435e837c93b9158b312
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-06-27 05:56:26 -0700

    shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sorts an array using shell sort
+ * @array: the array to be sorted
+ * @size: the size of the array
+ */
+
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, i, j;
+
+	gap = 1;
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+	while (gap > 0)
+	{
+		for (; gap >= 1; gap /= 3)
+		{
+			for (j = gap; j < size; j++)
+			{
+				i = j;
+				while (i >= gap && array[i - gap] > array[i])
+				{
+					swap(array + i, array + (i - gap));
+					i -= gap;
+				}
+			}
+		print_array(array, size);
+		}
+	}
+}
+
+/**
+ * swap - swaps two ints
+ * @a: first int
+ * @b: second int
+ */
+
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
